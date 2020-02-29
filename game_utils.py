5f4869f5919767643b1d5fd266912ed7af7eb59b
JorVLP <47005135+JorVLP@users.noreply.github.com>
2020-02-29 20:47:29 +0000

    get screen test cases

--- game_utils.py
+++ game_utils.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def get_screen(x,y,bgWidth, bgHeight, radius):
     possible = [i for i in range(4)]
@@ -6,7 +6,7 @@ def get_screen(x,y,bgWidth, bgHeight, radius):
     x = x % (2 * bgWidth) 
     y = y % (2 * bgHeight) 
 
-    if 0 <= x - radius and x + radius <= bgWidth:
+    if x - radius >= 0 and x + radius <= bgWidth:
         possible.remove(1)
         possible.remove(3)
 
@@ -14,7 +14,7 @@ def get_screen(x,y,bgWidth, bgHeight, radius):
         possible.remove(0)
         possible.remove(2)
 
-    if 0 <= y - radius and y + radius <= bgHeight:
+    if y - radius >= 0 and y + radius <= bgHeight:
         if 2 in possible: possible.remove(2)
         if 3 in possible: possible.remove(3)
 
@@ -38,3 +38,47 @@ def get_screen(x,y,bgWidth, bgHeight, radius):
         possible.remove(3)
         possible.append((3,x - bgWidth,y - bgHeight))
 
+    return possible
+
+if __name__ == '__main__':
+    bg = 100
+    test1 = get_screen(10,30,bg,bg,20)
+    output1 = [(0,10,30),(1,bg+10,30)]
+    if len(test1) != len(output1):
+        print("wrong length")
+    for t in test1:
+        if t not in output1:
+            print(f"wrong output1: {t}")
+
+    test2 = get_screen(bg-10,30,bg,bg,20)
+    output2 = [(0,bg-10,30),(1,-10,30)]
+    if len(test2) != len(output2):
+        print("wrong length")
+    for t in test2:
+        if t not in output2:
+            print(f"wrong output2: {t}")
+
+    test3 = get_screen(bg+10,30,bg,bg,20)
+    output3 = [(0,bg+10,30),(1,10,30)]
+    if len(test3) != len(output3):
+        print("wrong length")
+    for t in test3:
+        if t not in output3:
+            print(f"wrong output3: {t}")
+
+    test5 = get_screen(2*bg-10,30,bg,bg,20)
+    output5 = [(0,-10,30),(1,bg-10,30)]
+    if len(test5) != len(output5):
+        print("wrong length")
+    for t in test5:
+        if t not in output5:
+            print(f"wrong output5: {t}")
+
+    test4 = get_screen(50,50,bg,bg,20)
+    output4 = [(0,50,50)]
+    if len(test4) != len(output4):
+        print("wrong length")
+    for t in test4:
+        if t not in output4:
+            print(f"wrong output4: {t}")
+    
