486d4afde40ef2c736a556999b12bb16ce3d8b3d
Gabrielle Gaudeau <gabi@Gabrielles-MacBook-Pro.local>
2020-02-29 20:12:33 +0000

    menu first draft

---
+++ menu.py
@@ -0,0 +1,88 @@
+import pygame
+from pygame.locals import *
+import time
+import random
+import os
+
+# Game Initialization
+pygame.init()
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Game Resolution
+screen_width = 1024
+screen_height = 800
+screen=pygame.display.set_mode((screen_width, screen_height))
+
+# Colors
+white=(255, 255, 255)
+black=(0, 0, 0)
+gray=(50, 50, 50)
+red=(255, 0, 0)
+green=(0, 255, 0)
+blue=(0, 0, 255)
+yellow=(255, 255, 0)
+ 
+
+# Text Renderer
+def text_format(message, textFont, textSize, textColor):
+    newFont=pygame.font.SysFont(textFont, textSize)
+    newText=newFont.render(message, 0, textColor)
+    return newText
+
+
+# Game Fonts
+font = "arial"
+
+
+# Game Framerate
+clock = pygame.time.Clock()
+FPS=30
+
+# Main Menu
+def main_menu():
+ 
+    menu=True
+    selected="start"
+ 
+    while menu:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                    selected="start"
+                elif event.key==pygame.K_DOWN:
+                    selected="quit"
+                if event.key==pygame.K_RETURN:
+                    if selected=="start":
+                        print("Start")
+                    if selected=="quit":
+                        pygame.quit()
+                        quit()
+ 
+        # Main Menu UI
+        screen.fill(white)
+        title=text_format("Malfunctioning Penguin", font, 90, blue)
+        if selected=="start":
+            text_start=text_format("START", font, 75, blue)
+        else:
+            text_start = text_format("START", font, 75, black)
+        if selected=="quit":
+            text_quit=text_format("QUIT", font, 75, white)
+        else:
+            text_quit = text_format("QUIT", font, 75, black)
+ 
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
+ 
+        # Main Menu Text
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
+        pygame.display.update()
+        clock.tick(FPS)
+        pygame.display.set_caption("Python - Pygame Simple Main Menu Selection")
+
+main_menu()
