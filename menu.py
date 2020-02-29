1838472c1319c9fe7508994a1a58a8d62920fc9b
Gabrielle Gaudeau <gabi@Gabrielles-MacBook-Pro.local>
2020-02-29 21:30:45 +0000

    menu

--- menu.py
+++ menu.py
@@ -15,11 +15,11 @@ screen=pygame.display.set_mode((screen_width, screen_height))
 
 # Colors
 white=(255, 255, 255)
-black=(0, 0, 0)
+black=(33,54,72)
 gray=(50, 50, 50)
-red=(255, 0, 0)
+red=(234,65,123)
 green=(0, 255, 0)
-blue=(0, 0, 255)
+blue=(122,246,246)
 yellow=(255, 255, 0)
  
 
@@ -31,11 +31,11 @@ def text_format(message, textFont, textSize, textColor):
 
 
 # Game Fonts
-font = 'comicsans'
+font = 'Retro'
 
 
 # Game image
-penguin_img = pygame.transform.scale(pygame.image.load("./images/penguinTitle.png"), (500,500))
+penguin_img = pygame.transform.scale(pygame.image.load("./images/penguinTitle.png"), (460,460))
 
 
 # Game Framerate
@@ -68,13 +68,13 @@ def main_menu():
         # Main Menu UI
         screen.fill(white)
         title=text_format("Malfunctioning Penguin", font, 90, blue)
-        screen.blit(penguin_img, (262, 300))
+        screen.blit(penguin_img, (262, 120))
         if selected=="start":
-            text_start=text_format("START", font, 75, blue)
+            text_start=text_format("START", font, 75, red)
         else:
             text_start = text_format("START", font, 75, black)
         if selected=="quit":
-            text_quit=text_format("QUIT", font, 75, white)
+            text_quit=text_format("QUIT", font, 75, black)
         else:
             text_quit = text_format("QUIT", font, 75, black)
  
@@ -84,10 +84,25 @@ def main_menu():
  
         # Main Menu Text
         screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
-        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
-        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), screen_height // 2 + 150))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), screen_height // 2 + 250))
         pygame.display.update()
         clock.tick(FPS)
         pygame.display.set_caption("Python - Pygame Simple Main Menu Selection")
+
+        while True:
+            for event in pygame.event.get():
+                if(start_rect.collidepoint(pygame.mouse.get_pos())):
+                    pygame.quit()
+                    quit()
+                if(event.type == pygame.MOUSEBUTTONDOWN and event.start_rect== 1):
+                    pygame.quit()
+                    quit()
+                elif(event.type == pygame.MOUSEBUTTONDOWN and event.quit_rect==1):
+                    pygame.quit()
+                    quit()
+                elif(event.type == pygame.QUIT):
+                    pygame.quit()
+                    quit()
 
 main_menu()
