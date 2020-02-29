655047b946a1589d70a18d3b9c329ed44bd2d588
JorVLP <47005135+JorVLP@users.noreply.github.com>
2020-02-29 14:50:26 +0000

    add base game

---
+++ main.py
@@ -0,0 +1,171 @@
+import pygame
+from pygame.locals import *
+import os
+import random
+
+pygame.init()
+
+W, H = 600, 900
+win = pygame.display.set_mode((W,H))
+pygame.display.set_caption('Malfunctioning Penguin')
+
+bg = pygame.image.load(os.path.join('images', 'bg.png')).convert()
+bg_ystart = 0
+bg_yend = bg.get_width()
+
+clock = pygame.time.Clock()
+
+class player(object):
+    run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8, 16)]
+    jump = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(1, 8)]
+    slide = [pygame.image.load(os.path.join('images', 'S1.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S3.png')), pygame.image.load(os.path.join('images', 'S4.png')), pygame.image.load(os.path.join('images', 'S5.png'))]
+    fall = pygame.image.load(os.path.join('images', '0.png'))
+    jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.jumping = False
+        self.sliding = False
+        self.falling = False
+        self.slideCount = 0
+        self.jumpCount = 0
+        self.runCount = 0
+        self.slideUp = False
+
+    def draw(self, win):
+        if self.falling:
+            win.blit(self.fall, (self.x, self.y + 30))
+        elif self.jumping:
+            self.y -= self.jumpList[self.jumpCount] * 1.3
+            win.blit(self.jump[self.jumpCount//18], (self.x, self.y))
+            self.jumpCount += 1
+            if self.jumpCount > 108:
+                self.jumpCount = 0
+                self.jumping = False
+                self.runCount = 0
+            self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+        elif self.sliding or self.slideUp:
+            if self.slideCount < 20:
+                self.y += 1
+                self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+            elif self.slideCount == 80:
+                self.y -= 19
+                self.sliding = False
+                self.slideUp = True
+            elif self.slideCount > 20 and self.slideCount < 80:
+                self.hitbox = (self.x, self.y+3, self.width-8, self.height-35)
+
+            if self.slideCount >= 110:
+                self.slideCount = 0
+                self.runCount = 0
+                self.slideUp = False
+                self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+            win.blit(self.slide[self.slideCount//10], (self.x, self.y))
+            self.slideCount += 1
+
+        else:
+            if self.runCount > 42:
+                self.runCount = 0
+            win.blit(self.run[self.runCount//6], (self.x,self.y))
+            self.runCount += 1
+            self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-13)
+
+        #pygame.draw.rect(win, (255,0,0),self.hitbox, 2)
+
+def endScreen():
+    global pause, score, speed, obstacles
+    pause = 0
+    speed = 30
+    obstacles = []
+
+    run = True
+    while run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+                runner.falling = False
+                runner.sliding = False
+                runner.jumpin = False
+
+        win.blit(bg, (0,0))
+        largeFont = pygame.font.SysFont('comicsans', 80)
+        lastScore = largeFont.render('Best Score: ' + str(updateFile()),1,(255,255,255))
+        currentScore = largeFont.render('Score: '+ str(score),1,(255,255,255))
+        win.blit(lastScore, (W/2 - lastScore.get_width()/2,150))
+        win.blit(currentScore, (W/2 - currentScore.get_width()/2, 240))
+        pygame.display.update()
+    score = 0
+
+def redrawWindow():
+    largeFont = pygame.font.SysFont('comicsans', 30)
+    win.blit(bg, (bg_ystart, 0))
+    win.blit(bg, (bg_yend,0))
+    text = largeFont.render('Score: ' + str(score), 1, (255,255,255))
+    runner.draw(win)
+    for obstacle in obstacles:
+        obstacle.draw(win)
+
+    win.blit(text, (700, 10))
+    pygame.display.update()
+
+
+pygame.time.set_timer(USEREVENT+1, 500)
+pygame.time.set_timer(USEREVENT+2, 3000)
+speed = 30
+
+score = 0
+
+run = True
+runner = player(200, 313, 64, 64)
+
+obstacles = []
+pause = 0
+fallSpeed = 0
+
+while run:
+    if pause > 0:
+        pause += 1
+        if pause > fallSpeed * 2:
+            endScreen()
+
+    score = speed//10 - 3
+    pygame.draw.rect(bg,Color(255,0,0),Rect(0,0,1000,100))
+
+
+    bg_ystart -= 1.4
+    bg_yend -= 1.4
+
+    if bg_ystart < bg.get_width() * -1:
+        bg_ystart = bg.get_width()
+    if bg_yend < bg.get_width() * -1:
+        bg_yend = bg.get_width()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+
+        if event.type == USEREVENT+1:
+            speed += 1
+
+
+    if runner.falling == False:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+            if not(runner.jumping):
+                runner.jumping = True
+
+        if keys[pygame.K_DOWN]:
+            if not(runner.sliding):
+                runner.sliding = True
+
+    clock.tick(speed)
+    redrawWindow()
